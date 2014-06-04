84261fc5eb483c7738e004641f2239bfefea6f36
doranj <test>
2014-06-04 22:35:40 +1000

    Redirect form

--- admin/add_donators.php
+++ admin/add_donators.php
@@ -53,7 +53,7 @@ try {
 
             ?>
 
-            <form method = "post" action = "./add_donators?key=<?= $_GET['key'] ?>">
+            <form method = "post" action = "./add_donators.php?key=<?= $_GET['key'] ?>">
             <table border="1">
                 <tr>
                     <th>64bit Steam ID</th>
