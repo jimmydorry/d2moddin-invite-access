1f02842882b2072a54577569dcbbd9b1cf4f9db2
doranj <test>
2014-06-04 16:48:22 +1000

    second sandboxed button

--- donate/index.php
+++ donate/index.php
@@ -1,1 +1,5 @@
-<a href="https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_donations&business=DCCQ3EWFT6HWS&lc=AU&item_name=d2moddin&currency_code=AUD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted&return=http%3A%2F%2Fd2modd.in%2Fdonate%2Fipn.php">Donate here</a>
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=DCCQ3EWFT6HWS&lc=AU&item_name=d2moddin&currency_code=AUD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted&return=http%3A%2F%2Fd2modd.in%2Fdonate%2Fipn.php">Donate here</a>
+
+<br />
+
+<a href="https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_donations&business=NU6P6N82DAHS8&lc=US&item_name=d2moddin&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted&return=http%3A%2F%2Fd2modd.in%2Fdonate%2Fipn.php">Donate here</a>
