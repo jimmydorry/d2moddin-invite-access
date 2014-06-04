2786da914f7ac39e39237174b1d1e8fcb91fcb27
doranj <test>
2014-06-04 16:18:36 +1000

    Add donation landing page

---
+++ donate/index.php
@@ -0,0 +1,1 @@
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=DCCQ3EWFT6HWS&lc=AU&item_name=d2moddin&currency_code=AUD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted&return=http%3A%2F%2Fd2modd.in%2Fdonate%2Fipn.php">Donate here</a>
