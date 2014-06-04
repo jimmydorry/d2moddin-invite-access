5028cbbb2f0a7b43f3b4c1ae095d30d282638d30
doranj <test>
2014-06-04 21:30:29 +1000

    Bump cache time up to a minute

--- stats/index.php
+++ stats/index.php
@@ -56,7 +56,7 @@ try {
 
         $signup_stats = simple_cached_query('d2moddin_stats_queue_joins',
             'SELECT HOUR(date_invited) as hour, DAY(date_invited) as day, MONTH(date_invited) as month, YEAR(date_invited) as year, COUNT(*) as count FROM invite_key GROUP BY HOUR(date_invited), DAY(date_invited), MONTH(date_invited) ORDER BY 4,3,2,1;',
-            10);
+            60);
 
         $super_array = array();
         foreach ($signup_stats as $key => $value) {
