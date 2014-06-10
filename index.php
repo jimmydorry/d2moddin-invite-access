d389f679f678518e226cbc202371d25f2509036f
jimmydorry <jimmydorry+github@gmail.com>
2014-06-10 10:50:04 +1000

    PICNIC!!!

--- index.php
+++ index.php
@@ -44,14 +44,15 @@ if (!isset($_SESSION)) {
                         <h2 class="col-md-offset-5 animated delay023 fadeInBottom">Sign up to get your slot in the
                             beta!</h2>
 
-                        <?php $d2moddin_admins = simple_cached_query('d2moddin_admins'.$steamid64,
+                        <?php
+                        /*$d2moddin_admins = simple_cached_query('d2moddin_admins'.$steamid64,
                             "SELECT * FROM `admins` WHERE `steam_id` = $steamid64;",
                             30);
                         $d2moddin_admins = $d2moddin_admins[0];
 
                         if(!empty($d2moddin_admins)){
                             echo '<h2 class="col-md-offset-5 animated delay023 fadeInBottom"><a target="_new" href="http://d2modd.in/admin/?key='.$admin_pass_master.'">ADMIN PANEL</a></h2>';
-                        }
+                        }*/
                         ?>
 
                     </div>
