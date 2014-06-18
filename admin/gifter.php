39c74bbc69ee9410e00d3ae99e3b086805c505b4
doranj <test>
2014-06-19 02:42:40 +1000

    Updated gifter totals

--- admin/gifter.php
+++ admin/gifter.php
@@ -14,7 +14,7 @@ try {
         //CHECK ADMIN PASS
         if (!empty($admin_pass) && $admin_pass == $admin_pass_master) {
 
-            $gifter_users = $db->q("SELECT * FROM `invite_key` WHERE `gifter` = 1 ORDER BY queue_id ASC;");
+            $gifter_users = $db->q("SELECT ik.`queue_id`, ik.`steam_id`, ik.`date_invited`, (SELECT COUNT(*) FROM `invite_key` WHERE `steam_id` = ik.`steam_id`) as num_invites, (SELECT COUNT(*) FROM `invite_key` WHERE `steam_id` = ik.`steam_id` AND `activated` = 1) as num_accepted_invites FROM `invite_key` ik WHERE `gifter` = 1 ORDER BY queue_id ASC;");
 
             echo '<h1>Users with Ability to make Invite Codes</h1>';
             if (!empty($gifter_users)) {
@@ -22,13 +22,17 @@ try {
                 echo '<tr align="center">
                     <th>Queue ID</th>
                     <th>Steam ID</th>
+                    <th>Num Invites</th>
+                    <th>Num Invites Accepted</th>
                     <th>Date Joined</th>
                 </tr>';
                 foreach ($gifter_users as $key => $value) {
                     echo '<tr align="center">
                     <td>' . $value['queue_id'] . '</td>
                     <td><a href="http://steamcommunity.com/profiles/' . $value['steam_id'] . '" target="_new">' . $value['steam_id'] . '</a></td>
-                    <td>' . $value['date_invited'] . '</td>
+                    <td>' . $value['num_invites'] . '</td>
+                    <td>' . $value['num_accepted_invites'] . '</td>
+                    <td>' . relative_time(['date_invited']) . '</td>
                 </tr>';
                 }
                 echo '</table>';
