9ba24e449345b7dfb648515c580ea747682442e6
doranj <test>
2014-06-21 15:51:26 +1000

    Add minimum again

--- donate/IPN_noperks.php
+++ donate/IPN_noperks.php
@@ -82,14 +82,14 @@ if ($verified) {
                 //$donation_txn_id = $_POST['txn_id'];
                 //$donation_ipn_id = $_POST['ipn_track_id'];
 
-                //if($donation >= 0.99){
+                if($donation >= 0.99){
                     $invited = 1;
                     $permament = 1;
-                /*}
+                }
                 else{
                     $invited = 0;
                     $permament = 0;
-                }*/
+                }
 
                 $updateSQL = $db->q("INSERT INTO `invite_key` (`steam_id`, `donated`, `invited`, `permament`, `donation`, `donation_fee`, `donation_email`, `donation_txn_id`, `donation_ipn_id`) VALUES (?, 1, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `donated` = VALUES(`donated`), `invited` = VALUES(`invited`), `permament` = VALUES(`permament`), `donation` = VALUES(`donation`), `donation_fee` = VALUES(`donation_fee`), `donation_email` = VALUES(`donation_email`), `donation_txn_id` = VALUES(`donation_txn_id`), `donation_ipn_id` = VALUES(`donation_ipn_id`);",
                     'idiidsss',
